9559fc53d6ab9c4baa033ac2b1e140caafc29fa3
Emily Welsch <emwelsch@gmail.com>
2018-07-26 20:15:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,10 +25,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-    dog.id = row[0]
-    dog.name = row[1]
-    dog.breed = row[2]
-    dog.new(id, name, breed)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end
 
   def self.find_by_name
