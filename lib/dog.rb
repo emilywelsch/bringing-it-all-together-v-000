1fdcbfcf1bfa7b3cd4e2d529b5cfbac805fd5e0d
Emily Welsch <emwelsch@gmail.com>
2018-07-26 20:05:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,22 @@
 class Dog
-attr_accessor :name, :breed, :id
-# DB = {:conn => SQLite3::Database.new("db/dogs.db")}
+  attr_accessor :name, :breed, :id
+  # DB = {:conn => SQLite3::Database.new("db/dogs.db")}
 
-def initialize(id: nil, name:, breed:)
-  @id = id
-  @name = name
-  @breed = breed
-end
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
 
-def self.create_table
-  sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS dogs (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    breed TEXT,
-  )
-  SQL
-  DB[:conn].execute(sql)
-end
-
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
 
 end
